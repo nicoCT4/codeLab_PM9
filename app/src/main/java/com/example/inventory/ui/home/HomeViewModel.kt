064567344368c70be9a12b20e0964c1678b32a0b
Nicolás <con23197@uvg.edu.gt>
2024-10-05 23:17:20 -0600

    codelab terminado

--- app/src/main/java/com/example/inventory/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/inventory/ui/home/HomeViewModel.kt
@@ -1,18 +1,3 @@
-/*
- * Copyright (C) 2023 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 
 package com.example.inventory.ui.home
 
@@ -30,10 +15,6 @@ import kotlinx.coroutines.flow.stateIn
  */
 class HomeViewModel(itemsRepository: ItemsRepository) : ViewModel() {
 
-    /**
-     * Holds home ui state. The list of items are retrieved from [ItemsRepository] and mapped to
-     * [HomeUiState]
-     */
     val homeUiState: StateFlow<HomeUiState> =
         itemsRepository.getAllItemsStream().map { HomeUiState(it) }
             .stateIn(
@@ -47,7 +28,4 @@ class HomeViewModel(itemsRepository: ItemsRepository) : ViewModel() {
     }
 }
 
-/**
- * Ui State for HomeScreen
- */
 data class HomeUiState(val itemList: List<Item> = listOf())
